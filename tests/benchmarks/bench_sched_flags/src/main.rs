4586fdf523c5836ee8566f34baf3305d3613f123
Kaspar Schleiser <kaspar@schleiser.de>
2024-11-22 13:02:59 +0100

    fix(benchmarks): make bench_sched_flags do what RIOT-c version does

--- tests/benchmarks/bench_sched_flags/src/main.rs
+++ tests/benchmarks/bench_sched_flags/src/main.rs
@@ -13,24 +13,20 @@ static ID_EXCHANGE: Channel<ThreadId> = Channel::new();
 #[ariel_os::thread(autostart)]
 fn thread0() {
     let target_pid = ID_EXCHANGE.recv();
-    ID_EXCHANGE.send(&current_pid().unwrap());
 
-    match ariel_os::bench::benchmark(1000, || {
+    match ariel_os::bench::benchmark(10000, || {
         thread_flags::set(target_pid, 1);
-        thread_flags::wait_any(1);
     }) {
         Ok(ticks) => info!("took {} ticks per iteration", ticks),
         Err(_) => warn!("benchmark returned error"),
     }
 }
 
-#[ariel_os::thread(autostart)]
+#[ariel_os::thread(autostart, priority = 5)]
 fn thread1() {
     ID_EXCHANGE.send(&current_pid().unwrap());
-    let target_pid = ID_EXCHANGE.recv();
 
     loop {
-        thread_flags::set(target_pid, 1);
-        thread_flags::wait_any(1);
+        thread_flags::wait_any(0xffff);
     }
 }
