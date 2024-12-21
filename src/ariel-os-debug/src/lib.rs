8c8712f3d52f7386885f70f701fa1b3e50525e46
Kaspar Schleiser <kaspar@schleiser.de>
2024-12-21 15:34:19 +0100

    feat(threads): provide infini-core scheduler and native board
    Signed-off-by: Kaspar Schleiser <kaspar@schleiser.de>

--- src/ariel-os-debug/src/lib.rs
+++ src/ariel-os-debug/src/lib.rs
@@ -1,6 +1,6 @@
 //! Provides debug interface facilities.
 
-#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(any(test, context = "native")), no_std)]
 #![cfg_attr(test, no_main)]
 #![deny(missing_docs)]
 
@@ -42,6 +42,9 @@ impl ExitCode {
 pub fn exit(code: ExitCode) {
     #[cfg(feature = "semihosting")]
     semihosting::process::exit(code.to_semihosting_code());
+
+    #[cfg(feature = "std")]
+    std::process::exit(code.to_semihosting_code());
 
     #[allow(unreachable_code, reason = "stop nagging")]
     let _ = code;
@@ -175,6 +178,17 @@ pub mod backend {
     }
 }
 
+#[cfg(all(feature = "debug-console", feature = "std"))]
+mod backend {
+    pub use std::println;
+
+    #[doc(hidden)]
+    pub fn init() {
+        #[cfg(feature = "log")]
+        crate::logger::init();
+    }
+}
+
 #[cfg(not(feature = "debug-console"))]
 mod backend {
     #[doc(hidden)]
@@ -195,7 +209,7 @@ pub use backend::*;
 #[doc(hidden)]
 #[cfg(feature = "log")]
 mod logger {
-    use log::{Level, LevelFilter, Metadata, Record};
+    use log::{LevelFilter, Metadata, Record};
 
     static LOGGER: DebugLogger = DebugLogger;
 
@@ -252,17 +266,17 @@ mod logger {
             });
         }
 
-        log::trace!("debug logging enabled");
+        log::debug!("debug logging enabled at level {}", MAX_LEVEL);
     }
 
     struct DebugLogger;
 
     impl log::Log for DebugLogger {
-        fn enabled(&self, metadata: &Metadata) -> bool {
-            metadata.level() <= Level::Info
+        fn enabled(&self, metadata: &Metadata<'_>) -> bool {
+            metadata.level() <= MAX_LEVEL
         }
 
-        fn log(&self, record: &Record) {
+        fn log(&self, record: &Record<'_>) {
             if self.enabled(record.metadata()) {
                 crate::println!("[{}] {}", record.level(), record.args());
             }
