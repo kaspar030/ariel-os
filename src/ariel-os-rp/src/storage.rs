5cff4bb8ee1949c357589221fe13ab2f95a17fea
Kaspar Schleiser <kaspar@schleiser.de>
2026-03-23 23:04:31 +0100

    fix(rp): update use of flash API
    Signed-off-by: Kaspar Schleiser <kaspar@schleiser.de>

--- src/ariel-os-rp/src/storage.rs
+++ src/ariel-os-rp/src/storage.rs
@@ -1,13 +1,22 @@
-use embassy_rp::{flash::Async, peripherals::FLASH};
+use embassy_rp::{
+    bind_interrupts,
+    flash::Async,
+    peripherals::{DMA_CH1, FLASH},
+};
 
 pub type Flash = embassy_rp::flash::Flash<'static, FLASH, Async, FLASH_SIZE>;
 pub type FlashError = embassy_rp::flash::Error;
 
 const FLASH_SIZE: usize = 2 * 1024 * 1024;
 
+bind_interrupts!(struct Irqs {
+    DMA_IRQ_0 => embassy_rp::dma::InterruptHandler<DMA_CH1>;
+});
+
 pub fn init(p: &mut crate::OptionalPeripherals) -> Flash {
     embassy_rp::flash::Flash::<_, Async, FLASH_SIZE>::new(
         p.FLASH.take().unwrap(),
         p.DMA_CH1.take().unwrap(),
+        Irqs,
     )
 }
